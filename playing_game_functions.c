ebb7e5c57ac75235e56fa8e8fc3bd6ac23e022c0
MinJijem <55428081+MinJijem@users.noreply.github.com>
2019-11-12 21:23:02 +0900

    P_G_F

--- playing_game_functions.c
+++ playing_game_functions.c
@@ -5,6 +5,7 @@
 #define N_DOLLAR			50
 
 #define N_MAX_USER			5
+#define N_MAX_BET			5
 
 
 
@@ -20,7 +21,7 @@ int configUser(){
 	do
 	{
 	printf("Input the number of players (MAX:5) : ");
-	n_user=getIntegerInput();
+	scanf("%d",&n_user);
 	
 	if(n_user>5)
 		printf("Too many player!\n");
@@ -36,11 +37,10 @@ int betDollar(void) {
 	int r;
 	
 	printf("\n--------- BETTING STRP ---------\n");
-	//user betting
 	do{
 		
-	printf("  -> your betting (total:$%d) :$ ",dollar[0]);
-	bet[0]=getIntegerInput();
+	printf("  -> your betting (total:$%d) :$",dollar[0]);
+	scanf("%d",&bet[0]);
 	
 	if(bet[0]<0)
 		printf("  -> invald input for betting ($%d)\n",bet[0]);
@@ -48,9 +48,9 @@ int betDollar(void) {
 		printf("  -> you only have $%d! bet again\n",dollar[0]);
 	}while(bet[0]<0 || bet[0]>dollar[0]);
 	
-	for(i=1;i<n_user;i++) //plaers betting
+	for(i=1;i<n_user;i++)
 	{
-		r=1+rand() % dollar[i];
+		r=1+rand() % N_MAX_BET;
 		bet[i]=r;
 		printf("  -> player%d bets $%d (out of $%d)\n",i,bet[i],dollar[i]);
 	}
